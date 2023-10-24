66cbc1663d3ca7ade7ca030bb967a335036c8d17
SuryaRf <141994348+SuryaRf@users.noreply.github.com>
2023-10-24 15:01:49 +0700

    Add files via upload

---
+++ Jobsheet9/LinearSearch27_1.java
@@ -0,0 +1,38 @@
+package Jobsheet9;
+
+import java.util.Scanner;
+
+public class LinearSearch27_1 {
+    public static void main(String[] args) {
+
+        Scanner sc27 = new Scanner(System.in);
+
+        int jmlInput;
+        int[] arrayInt;
+        int key;
+        int hasil = 0;
+
+        System.out.print("Masukan jumlah elemen array: ");
+        jmlInput = sc27.nextInt();
+
+        arrayInt = new int[jmlInput];
+
+        for (int i = 0; i < jmlInput; i++) {
+            System.out.print("Masukan nilai elemen ke- " + (i + 1) + ": ");
+            arrayInt[i] = sc27.nextInt();
+        }
+
+        System.out.print("Masukan key yang ingin dicari: ");
+        key = sc27.nextInt();
+
+        for (int i = 0; i < arrayInt.length; i++) {
+            if (arrayInt[i] == key) {
+                hasil = i;
+                break;
+            }
+        }
+
+        System.out.println("Key ada dalam array pada posisi indeks ke- " + hasil);
+
+    }
+}
